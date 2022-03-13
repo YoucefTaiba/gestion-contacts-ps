2e95d53efc85901353a4274bef9c8c6ba6ed9ca7
YoucefTaiba <youcef.taiba@yahoo.fr>
2022-03-13 02:08:43 +0100

    Integration front end

--- src/main/java/com/poliscrypts/project/GestionContactePsApplication.java
+++ src/main/java/com/poliscrypts/project/GestionContactePsApplication.java
@@ -1,7 +1,14 @@
 package com.poliscrypts.project;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class GestionContactePsApplication {
@@ -10,4 +17,15 @@ public class GestionContactePsApplication {
 		SpringApplication.run(GestionContactePsApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFiltre() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+		configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return new CorsFilter(source);
+	}
 }
