bfdc4b3d2ef49f95fafd058dce77f0eb9231a2fc
Taiba Youcef <youcef.taiba@yahoo.fr>
2022-03-20 09:06:20 +0100

    changement d'interface com.auth0.jwt.interfaces.JWTVerifier

--- src/main/java/com/poliscrypts/project/controller/UserRessource.java
+++ src/main/java/com/poliscrypts/project/controller/UserRessource.java
@@ -19,10 +19,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poliscrypts.project.model.Role;
 import com.poliscrypts.project.model.User;
