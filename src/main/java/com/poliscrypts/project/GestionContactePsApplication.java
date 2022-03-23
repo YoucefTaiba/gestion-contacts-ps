7b1ebf27412314e88b39441272fecce2fb635f87
Taiba Youcef <youcef.taiba@yahoo.fr>
2022-03-23 11:51:18 +0100

    Fix BUG CORS

--- src/main/java/com/poliscrypts/project/GestionContactePsApplication.java
+++ src/main/java/com/poliscrypts/project/GestionContactePsApplication.java
@@ -1,15 +1,10 @@
 package com.poliscrypts.project;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter; 
+import org.springframework.security.crypto.password.PasswordEncoder;  
 
 @SpringBootApplication
 public class GestionContactePsApplication { 
@@ -22,17 +17,5 @@ public class GestionContactePsApplication {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	} 
 	}
-	@Bean
-	public CorsFilter corsFiltre() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-		configuration.setAllowCredentials(true);
-		configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Origin","Authorization", "Content-Type,", "Origin", "Accept"));
-		configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin","Authorization", "Content-Type,", "Origin", "Accept","x-auth-token"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return new CorsFilter(source);
-	}
-}
--- src/main/java/com/poliscrypts/project/security/WebSecurityConfig.java
+++ src/main/java/com/poliscrypts/project/security/WebSecurityConfig.java
@@ -1,18 +1,21 @@
 package com.poliscrypts.project.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource; 
 
 @Configuration
 //@EnableWebSecurity
@@ -57,6 +60,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		httpSecurity.addFilter(customAuthentificationFilter);
 		httpSecurity.addFilterBefore(new CustomAuthorisationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
- 
+	 @Bean
+	    public CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.setAllowedOrigins(Arrays.asList("*"));
+	        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+	        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+	        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+	         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration);
+	        return source;
+	    }
 	
 }
