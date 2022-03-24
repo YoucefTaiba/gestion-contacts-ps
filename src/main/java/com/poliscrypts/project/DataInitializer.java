b7c0d25d0f788e0c6d4ce3458182ab7fd58c8560
Taiba Youcef <youcef.taiba@yahoo.fr>
2022-03-24 13:27:33 +0100

    mise a jour

--- src/main/java/com/poliscrypts/project/DataInitializer.java
+++ src/main/java/com/poliscrypts/project/DataInitializer.java
@@ -4,17 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import com.poliscrypts.project.model.Company;
 import com.poliscrypts.project.model.Role;
 import com.poliscrypts.project.model.User;
+import com.poliscrypts.project.service.CompanyService;
 import com.poliscrypts.project.service.UserServiceImpl;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 	private final UserServiceImpl userRepository;
+	private final CompanyService companyService;
 
 	@Autowired
-	public DataInitializer(UserServiceImpl userRepository) {
+	public DataInitializer(UserServiceImpl userRepository,CompanyService companyService) {
 		this.userRepository = userRepository;
+		this.companyService=companyService;
 	}
 
 	@Override
@@ -26,6 +30,8 @@ public class DataInitializer implements CommandLineRunner {
 		userRepository.addRoleToUser("taiba", "ROLE_MANAGER");
 		userRepository.addRoleToUser("taiba", "ROLE_USER");
 		userRepository.addRoleToUser("bessaha", "ROLE_USER");
+		companyService.addCompany(new Company("company1","adress",.19F));
+		companyService.addCompany(new Company("company2","adress",.17F));
 
 	}
 
